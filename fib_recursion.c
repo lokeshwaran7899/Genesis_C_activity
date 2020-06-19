e2995e1ace20f7f07432579c43f37aaf0824f0e8
lokeshwaran7899 <67134232+lokeshwaran7899@users.noreply.github.com>
2020-06-19 08:55:09 +0530

    Fibonacci series using recursion function
    Fibonacci series using recursion function
    The execution time is 1.859s for 20 values
    The complexity if program is higher than previous code

---
+++ fib_recursion.c
@@ -0,0 +1,27 @@
+//Fibonacci Series using Recursion
+#include<stdio.h>
+void fib(int );
+int main()
+{
+    int k, n;
+    long int i = 0, j = 1;
+    printf("number of terms: ");
+    scanf("%d", &n);
+    printf("\nSeries\t",n);
+    printf("%d ", 1);
+    fib(n);
+    return 0;
+}
+
+void fib(int aj)
+{
+    static long int first = 0, second = 1, sum;
+    if(aj > 1)
+    {
+        sum = first + second;
+        first = second;
+        second = sum;
+        printf("%ld ", sum);
+        fib(aj-1);
+    }
+}
