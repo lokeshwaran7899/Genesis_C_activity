60906fff29910eb7b22af75a81d17af559996135
lokeshwaran7899 <67134232+lokeshwaran7899@users.noreply.github.com>
2020-06-19 10:11:18 +0530

    Update fib_for_loop.c

--- fib_for_loop.c
+++ fib_for_loop.c
@@ -1,13 +1,13 @@
 //Fibonacci Series using for loop
 #include <stdio.h>
 int main() {
-    int i, n, t1 = 0, t2 = 1, t3;
+    int i, n, t1 = 0, t2 = 1;
     printf("number of terms: ");
     scanf("%d", &n);
     printf("\nSeries: ");
     for (i = 1; i <= n; ++i) {
         printf("%d, ", t1);
-        t3 = t1 + t2;
+        int t3 = t1 + t2;
         t1 = t2;
         t2 = t3;
     }
