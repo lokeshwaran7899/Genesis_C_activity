a49e483e485447e6a6b6b7f09970e809d0da0766
lokeshwaran7899 <67134232+lokeshwaran7899@users.noreply.github.com>
2020-06-19 08:48:21 +0530

    Add files via upload
    Fibonacci series using for loop
    execution time for 20 values is 2.455s

---
+++ fib_for_loop.c
@@ -0,0 +1,16 @@
+//Fibonacci Series using for loop
+#include <stdio.h>
+int main() {
+    int i, n, t1 = 0, t2 = 1, t3;
+    printf("number of terms: ");
+    scanf("%d", &n);
+    printf("\nSeries: ");
+    for (i = 1; i <= n; ++i) {
+        printf("%d, ", t1);
+        t3 = t1 + t2;
+        t1 = t2;
+        t2 = t3;
+    }
+
+    return 0;
+}
